6795cef8592c8808b1313b5baecb0c1faa2321aa
LaranGod <miguel05amancio2000@gmail.com>
2022-10-25 22:04:04 -0300

    Update styling

--- pages/index.js
+++ pages/index.js
@@ -24,87 +24,89 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className="mb-10 text-xl">ANÁLISE DE DECISÃO</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="ambienteDecisao">
-            <p className="text-md">Qual o ambiente de decisão?</p>
+        <div className="h-[600px] w-[500px] flex items-center flex-col border-4 pt-16">
+          <h1 className="mb-10 text-xl">ANÁLISE DE DECISÃO</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="ambienteDecisao">
+              <p className="text-md">Qual o ambiente de decisão?</p>
 
-            <div className="flex gap-4 mb-4">
-              <div className="radio">
-                <label>
+              <div className="flex gap-4 mb-10">
+                <div className="radio">
+                  <label>
+                    <input
+                      {...register("ambienteDecisao", {
+                        required: "Selecione o ambiente de decisão!",
+                      })}
+                      type="radio"
+                      value="Risco"
+                      className="mr-2"
+                    />
+                    Risco
+                  </label>
+                </div>
+                <div className="radio">
+                  <label>
+                    <input
+                      {...register("ambienteDecisao")}
+                      type="radio"
+                      value="Incerteza"
+                      className="mr-2"
+                    />
+                    Incerteza
+                  </label>
+                </div>
+              </div>
+
+              <ErrorMessage
+                errors={errors}
+                name="ambienteDecisao"
+                render={({ message }) => (
+                  <p className="text-sm text-red-500 mb-4">{message}</p>
+                )}
+              />
+            </div>
+            <div className="cenarios">
+              <p className="text-md">Qual o número de cenários?</p>
+
+              <div className="flex mb-4">
+                <div className="input">
                   <input
-                    {...register("ambienteDecisao", {
-                      required: "Selecione o ambiente de decisão!",
+                    className="border-2 h-8"
+                    type="number"
+                    min='0'
+                    {...register("numCenarios", {
+                      valueAsNumber: true,
                     })}
-                    type="radio"
-                    value="Risco"
-                    className="mr-2"
                   />
-                  Risco
-                </label>
+                </div>
               </div>
-              <div className="radio">
-                <label>
+            </div>
+            <div className="mb-28">
+              <p className="text-md">Qual o número de Investimentos?</p>
+
+              <div className="flex mb-4">
+                <div className="">
                   <input
-                    {...register("ambienteDecisao")}
-                    type="radio"
-                    value="Incerteza"
-                    className="mr-2"
+                    className="border-2 h-8"
+                    type="number"
+                    min='0'
+                    {...register("numInvestimentos", {
+                      valueAsNumber: true,
+                    })}
                   />
-                  Incerteza
-                </label>
+                </div>
               </div>
             </div>
 
-            <ErrorMessage
-              errors={errors}
-              name="ambienteDecisao"
-              render={({ message }) => (
-                <p className="text-sm text-red-500 mb-4">{message}</p>
-              )}
-            />
-          </div>
-          <div className="cenarios">
-            <p className="text-md">Qual o número de cenários?</p>
-
-            <div className="flex mb-4">
-              <div className="input">
-                <input
-                  className="border-2"
-                  type="number"
-                  min='0'
-                  {...register("numCenarios", {
-                    valueAsNumber: true,
-                  })}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="investimentos">
-            <p className="text-md">Qual o número de Investimentos?</p>
-
-            <div className="flex mb-4">
-              <div className="">
-                <input
-                  className="border-2"
-                  type="number"
-                  min='0'
-                  {...register("numInvestimentos", {
-                    valueAsNumber: true,
-                  })}
-                />
-              </div>
-            </div>
-          </div>
-
-          <button
-            type="submit"
-            className="border rounded border-green-500 bg-green-500 text-white text-sm p-2"
-          >
-            Prosseguir
-          </button>
-          {console.log("errors", errors)}
-        </form>
+            <button
+              type="submit"
+              className="border rounded border-green-500 bg-green-500 text-white text-sm w-full p-2"
+            >
+              Prosseguir
+            </button>
+            {console.log("errors", errors)}
+          </form>
+        </div>
       </main>
     </div>
   );
